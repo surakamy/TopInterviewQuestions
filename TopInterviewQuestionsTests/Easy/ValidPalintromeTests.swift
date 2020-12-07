c69c25affbc758ebb7cd79e9114044902058ec7a
Dmytro Kholodov <surakamy@gmail.com>
2020-12-07 20:32:19 +0200

    Add Valid Palindrome problem setup

---
+++ TopInterviewQuestionsTests/Easy/ValidPalintromeTests.swift
@@ -0,0 +1,26 @@
+//
+//  ValidPalintromeTests.swift
+//  TopInterviewQuestionsTests
+//
+//  Created by Dmytro Kholodov on 07.12.2020.
+//
+// https://leetcode.com/problems/valid-palindrome/
+
+import XCTest
+
+/*
+ Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+ Note: For the purpose of this problem, we define empty string as valid palindrome.
+
+ Constraints:
+ s consists only of printable ASCII characters.
+
+ */
+
+func isPalindrome(_ s: String) -> Bool {
+    false
+}
+
+class ValidPalintromeTests: XCTestCase {
+
+}
