48e3b8cb9d88983fe9df549e407e0f520527bb1c
Dmytro Kholodov <surakamy@gmail.com>
2020-12-08 08:43:43 +0200

    Add Climbing Stairs setup

---
+++ TopInterviewQuestionsTests/Easy/ClibmingStairsTests.swift
@@ -0,0 +1,26 @@
+//
+//  ClibmingStairsTests.swift
+//  TopInterviewQuestionsTests
+//
+//  Created by Dmytro Kholodov on 08.12.2020.
+//
+// https://leetcode.com/problems/climbing-stairs/
+
+import XCTest
+
+/*
+ You are climbing a staircase. It takes n steps to reach the top.
+
+ Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+ Constraints:
+    1 <= n <= 45
+ */
+
+func climbStairs(_ n: Int) -> Int {
+    0
+}
+
+class ClibmingStairsTests: XCTestCase {
+
+}
