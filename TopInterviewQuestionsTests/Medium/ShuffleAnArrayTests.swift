abde780915dacc6b8be79b9714bfcccf0298eeb1
Dmytro Kholodov <surakamy@gmail.com>
2020-12-11 15:08:43 +0200

    Add setup for Shuffle an Array problem

---
+++ TopInterviewQuestionsTests/Medium/ShuffleAnArrayTests.swift
@@ -0,0 +1,45 @@
+//
+//  ShuffleAnArrayTests.swift
+//  TopInterviewQuestionsTests
+//
+//  Created by Dmytro Kholodov on 11.12.2020.
+//
+// https://leetcode.com/problems/shuffle-an-array/
+
+import XCTest
+
+/*Given an integer array nums, design an algorithm to randomly shuffle the array.
+Implement the Solution class:
+
+Solution(int[] nums) Initializes the object with the integer array nums.
+int[] reset() Resets the array to its original configuration and returns it.
+int[] shuffle() Returns a random shuffling of the array.
+
+Constraints:
+    1 <= nums.length <= 200
+    -106 <= nums[i] <= 106
+    All the elements of nums are unique.
+    At most 5 * 104 calls will be made to reset and shuffle.*/
+
+
+class Solution {
+
+    init(_ nums: [Int]) {
+
+    }
+
+    /** Resets the array to its original configuration and return it. */
+    func reset() -> [Int] {
+        []
+    }
+
+    /** Returns a random shuffling of the array. */
+    func shuffle() -> [Int] {
+        []
+    }
+}
+
+
+class ShuffleAnArrayTests: XCTestCase {
+
+}
